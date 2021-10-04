4d85077dc61d1afeae9b3253f89f25519d98098b
Reimnop <56994757+Reimnop@users.noreply.github.com>
2021-10-04 10:15:58 +0700

    Update DebugMenu.cpp

--- ArrhythmiaStudio/src/logic/DebugMenu.cpp
+++ ArrhythmiaStudio/src/logic/DebugMenu.cpp
@@ -14,15 +14,9 @@ DebugMenu::DebugMenu()
 	ImGuiController::onLayout.push_back(std::bind(&DebugMenu::onLayout, this));
 }
 
-void YUVfromRGB(double& Y, double& U, double& V, const double R, const double G, const double B)
-{
-	Y = 0.257 * R + 0.504 * G + 0.098 * B + 16;
-	U = -0.148 * R - 0.291 * G + 0.439 * B + 128;
-	V = 0.439 * R - 0.368 * G - 0.071 * B + 128;
-}
-
 void DebugMenu::onLayout()
 {
+	const LevelManager* levelManager = LevelManager::inst;
 	lastTime += MainWindow::inst->deltaTime;
 	framerate++;
 
@@ -46,6 +40,9 @@ void DebugMenu::onLayout()
 
 		const std::string frametimeStr = "Frametime: " + std::to_string_with_precision(MainWindow::inst->deltaTime * 1000.0f, 2) + "ms";
 		ImGui::Text(frametimeStr.c_str());
+
+		const std::string objectsCount = "Objects count: " + std::to_string(levelManager->level->levelObjects.size());
+		ImGui::Text(objectsCount.c_str());
 
 		ImGui::SliderInt("Swap Interval", &interval, 0, 4);
 		if (ImGui::IsItemEdited())

9a6a0057e3330046b5dcd71025e0925606c43b1f
Reimnop <56994757+Reimnop@users.noreply.github.com>
2022-01-01 16:38:41 +0700

    Made VideoExporter memory safe

--- ArrhythmiaStudio/src/logic/VideoExporter.h
+++ ArrhythmiaStudio/src/logic/VideoExporter.h
@@ -1,21 +1,35 @@
 #pragma once
 
 #include <filesystem>
-#include <ffmpegcpp.h>
+
+#include "ffmpegcpp.h"
+#include "log4cxx/logger.h"
+
+using namespace std::filesystem;
 
 class VideoExporter
 {
 public:
-	VideoExporter(int width, int height, int framerate, int startFrame, int endFrame, ffmpegcpp::VideoCodec* videoCodec, ffmpegcpp::AudioCodec* audioCodec);
-	~VideoExporter();
+	VideoExporter(int width, int height, int framerate, int startFrame, int endFrame, const char* videoCodec, const char* audioCodec) :
+		width(width),
+		height(height),
+		framerate(framerate),
+		startFrame(startFrame),
+		endFrame(endFrame),
+		videoCodec(videoCodec),
+		audioCodec(audioCodec)
+	{
+	}
 	
-	void exportToVideo(std::filesystem::path path) const;
+	void exportToVideo(path path) const;
 private:
+	static inline log4cxx::LoggerPtr logger = log4cxx::Logger::getLogger("VideoExporter");
+
 	int width;
 	int height;
 	int framerate;
 	int startFrame;
 	int endFrame;
-	ffmpegcpp::VideoCodec* videoCodec;
-	ffmpegcpp::AudioCodec* audioCodec;
+	ffmpegcpp::VideoCodec videoCodec;
+	ffmpegcpp::AudioCodec audioCodec;
 };
